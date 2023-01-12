08bb931ead2154793598ac4225cd3c9d42f38a9e
Pranjal <jainpranjal30112002@gmail.com>
2023-01-12 17:20:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+
+export default function App() {
+  return (
+    <div>
+      <NavBar/>
+      <News pageSize={6}/>
+    </div>
+  )
+}
