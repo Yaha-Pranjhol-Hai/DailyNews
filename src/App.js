35052c9b893547af162c033368fd74ae7c855498
Pranjal <jainpranjal30112002@gmail.com>
2023-01-13 11:02:37 +0530

    changed function component to class component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import React from 'react'
+import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 
-export default function App() {
+export default class App extends Component {
+  render(){
   return (
     <div>
       <NavBar/>
       <News pageSize={6} country="in" category="science"/>
     </div>
   )
+}
 }
