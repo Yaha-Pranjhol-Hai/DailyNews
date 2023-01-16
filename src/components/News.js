cb8a0ca652cd8d1040fad793b99c8797e29a5c1b
Pranjal <jainpranjal30112002@gmail.com>
2023-01-16 14:00:03 +0530

    tutorial 34 complete.

--- src/components/News.js
+++ src/components/News.js
@@ -16,14 +16,19 @@ export class News extends Component {
     category: PropTypes.string,
   }
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string)=> {
+    return string.charAt(0).toUpperCase() +string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     console.log("Hello i am a constructor from News component");
     this.state = {
       articles: [],
       loading: false,
       page: 1
     }
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
 
   async updateNews(){
@@ -51,7 +56,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center style={{margin: '35px 0px}}">NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center" style={{margin: '35px 0px'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner/>}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
