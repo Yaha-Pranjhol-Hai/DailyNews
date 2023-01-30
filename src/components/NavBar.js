5bba7f7d255b89c0a318ab07453b7022ffc30229
Pranjal <jainpranjal30112002@gmail.com>
2023-01-30 15:17:12 +0530

    Some Changes.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     <div>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/NewsMonkey">
             NewsMonkey
           </Link>
           <button
